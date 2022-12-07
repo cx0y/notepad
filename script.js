888eb79816663f884fdecd76374db455f7be4838
wiz <rohit02sadhukhan@gmail.com>
2022-12-07 22:17:48 +0530

    update

--- script.js
+++ script.js
@@ -8,7 +8,32 @@ computers for practical applications, larger realizations are
 believed to be capable of solving certain computational problems,
 such as integer factorization (which underlies RSA encryption),
 substantially faster than classical computers. The study of quantum
-computing is a subfield of quantum information science.`
+computing is a subfield of quantum information science.`;
 
+const saveTextAsFile = () => {
+  const textToWrite = document.getElementById("pad").value;
+  const textFileAsBlob = new Blob([textToWrite], { type: "text/plain" });
+  const fileNameToSaveAs = "file.txt"; //filename.extension
+  const downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL != null) {
+    // Chrome
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    //Firefox
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+  downloadLink.click();
+};
+
+document.onkeydown = (e) => {
+    if(e.ctrlKey && e.key === "q"){
+        saveTextAsFile();
+    }
+};
 
 
