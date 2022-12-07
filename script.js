7ccb3b2e5f1b04e361cc593da456eeaf9be7de79
wiz <rohit02sadhukhan@gmail.com>
2022-12-07 23:45:22 +0530

    update

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const pad = document.getElementById("pad");
-pad.placeholder = `Quantum computing is a type of computation whose operations can
-harness the phenomena of quantum mechanics, such as superposition,
-interference, and entanglement. Devices that perform quantum
-computations are known as quantum computers.[1][2] Though current
-quantum computers are too small to outperform usual (classical)
-computers for practical applications, larger realizations are
-believed to be capable of solving certain computational problems,
-such as integer factorization (which underlies RSA encryption),
-substantially faster than classical computers. The study of quantum
-computing is a subfield of quantum information science.`;
+pad.placeholder = `Quantum computing is a type of computation whose operations can ....`;
+
 
 const saveTextAsFile = () => {
   const textToWrite = document.getElementById("pad").value;
@@ -31,9 +23,7 @@ const saveTextAsFile = () => {
 };
 
 document.onkeydown = (e) => {
-    if(e.ctrlKey && e.key === "q"){
-        saveTextAsFile();
-    }
+  if (e.ctrlKey && e.key === "q") {
+    saveTextAsFile();
+  }
 };
-
-
